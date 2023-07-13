b0881f94447684cfa49cbbe7c21e0ca814a00cbb
Damir Kabirov <44746689+Damir-Kabirov@users.noreply.github.com>
2023-07-13 22:20:20 +0300

    finish modification

--- app.js
+++ app.js
@@ -1,5 +1,44 @@
 const cards =document.querySelector('.cards')
 const allCart = cards.querySelectorAll('.card-hero')
+const items =  fetch('data.json').then(res=>res.json()).then((data)=>{return data.hero})
+const skilsContainer = document.querySelector('.skils')
+const burger = document.querySelector('.burder-menu')
+const menu = document.querySelector('.nav')
+skilsContainer.innerHTML =''
+
+function renderSkill(heroes,id){
+    heroes.forEach(hero=>{
+        if(hero.name===id){
+            getSkil(hero)
+        }
+    })
+}
+
+function getSkil(hero) {
+    let html =''
+    const skils = hero.skil
+    skils.map(skil=> {
+      html+=toCard(skil)
+    } )
+   skilsContainer.innerHTML=html
+}
+function toCard(skil){
+
+    return `
+    <div class="skil-cart">
+    <div class="skil__info">
+      <p class="skil__type">Skill</p>
+      <h5 class="skil__name">${skil.name}</h5>
+      <p class="skil__discription">${skil.disrtiption}</p>
+    </div>
+    <div class="skil__logo">
+      ${skil.img}
+    </div>
+  </div>
+    `
+    
+}
+
 cards.addEventListener('click',(e)=>{
     const parent = e.target.closest('div')
     const cart = e.target.closest('.card-hero')
@@ -13,6 +52,17 @@ cards.addEventListener('click',(e)=>{
                 cartName.classList.add('hero__name--active')
             }
        });
+       items.then(arr=>{
+        console.log(cart.id)
+        renderSkill(arr,cart.id)
+    })
+
     }
     
 })
+burger.addEventListener('click',()=>{
+  menu.classList.toggle('db')
+})
+
+
+
